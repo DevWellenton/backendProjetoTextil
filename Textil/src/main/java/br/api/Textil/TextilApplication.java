392672dbe78ad75fca1e690bd2a4b6c3613f06b9
DevWellenton <97131101+DevWellenton@users.noreply.github.com>
2023-03-06 21:26:46 -0300

    Inicializando projeto

---
+++ Textil/src/main/java/br/api/Textil/TextilApplication.java
@@ -0,0 +1,13 @@
+package br.api.Textil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextilApplication.class, args);
+	}
+
+}
