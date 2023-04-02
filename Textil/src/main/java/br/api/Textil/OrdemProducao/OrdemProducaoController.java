62868eb58ebb8b85c61393113f89334370916ce6
wellenton <wellenton16@hotmail.com>
2023-04-02 12:10:23 -0300

    Alterações em: OrdemProducaoController OrdemProducaoRepository OrdemProducaoRepresentation

--- Textil/src/main/java/br/api/Textil/OrdemProducao/OrdemProducaoController.java
+++ Textil/src/main/java/br/api/Textil/OrdemProducao/OrdemProducaoController.java
@@ -1,4 +1,77 @@
 package br.api.Textil.OrdemProducao;
 
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
 public class OrdemProducaoController {
+
+    private OrdemProducaoService ordemProducaoService;
+    private OrdemProducaoRepository ordemProducaoRepository;
+
+    @PostMapping("/")
+    public ResponseEntity<OrdemProducaoRepresentation.Detalhes> createOrdemProducao(
+            @RequestBody @Valid OrdemProducaoRepresentation.CriarOuAtualizar criar){
+
+        OrdemProducao ordemProducao = this.OrdemProducaoService.criarOrdemProducao(criar);
+
+        OrdemProducaoRepresentation.Detalhes detalhes =
+                OrdemProducaoRepresentation.Detalhes.from(ordemProducao);
+
+        return ResponseEntity.ok(detalhes);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<OrdemProducaoRepresentation.Lista>> buscarOrdensProducao(
+            @QuerydslPredicate(root = OrdemProducao.class) Predicate filtroURI,
+            @RequestParam(name="tamanhoPagina", defaultValue = "30") int tamanhoPagina,
+            @RequestParam(name = "paginaSelecionada", defaultValue = "0") int paginaSelecionada) {
+
+
+        Pageable pageable = PageRequest.of(paginaSelecionada, tamanhoPagina);
+
+        Page<OrdemProducao> ordemProducaoList = Objects.isNull(filtroURI) ?
+                this.ordemProducaoService.buscarTodos(pageable) :
+                this.ordemProducaoService.buscarTodos(filtroURI, pageable);
+
+        List<OrdemProducaoRepresentation.Lista> listaFinal =
+                OrdemProducaoRepresentation.Lista.from(ordemProducaoList.getContent());
+
+        return ResponseEntity.ok(listaFinal);
+    }
+
+    @PutMapping("/{idOrdemProducao}")
+    public ResponseEntity<OrdemProducaoRepresentation.Detalhes> atualizarOrdemProducao(
+            @PathVariable Long idOrdemProducao,
+            @RequestBody OrdemProducaoRepresentation.CriarOuAtualizar atualizar) {
+
+        OrdemProducao ordemProducaoAtualizado =
+                this.ordemProducaoService.atualizar(idOrdemProducao, atualizar);
+
+        OrdemProducaoRepresentation.Detalhes detalhes =
+                OrdemProducaoRepresentation.Detalhes
+                        .from(ordemProducaoAtualizado);
+
+        return ResponseEntity.ok(detalhes);
+    }
+
+    @GetMapping("/{idOrdemProducao}")
+    public ResponseEntity<OrdemProducaoRepresentation.Detalhes> buscarUmaOrdemProducao(
+            @PathVariable Long idOrdemProducao) {
+
+        OrdemProducao ordemProducao = this.ordemProducaoService.buscarUmaOrdemProducao(idOrdemProducao);
+
+        OrdemProducaoRepresentation.Detalhes detalhes =
+                OrdemProducaoRepresentation.Detalhes
+                        .from(ordemProducao);
+
+        return ResponseEntity.ok(detalhes);
+    }
 }
