91f02e46296966201a731233b36dfcc419b37789
wellenton <wellenton16@hotmail.com>
2023-04-08 14:13:50 -0300

    Feito as ligações entre as tabelas.

--- Textil/src/main/java/br/api/Textil/OrdemProducao/OrdemProducao.java
+++ Textil/src/main/java/br/api/Textil/OrdemProducao/OrdemProducao.java
@@ -1,5 +1,7 @@
 package br.api.Textil.OrdemProducao;
 
+import br.api.Textil.Terceiro.Terceiro;
+import br.api.Textil.Usuario.Usuario;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -39,4 +41,10 @@ public class OrdemProducao {
     @Size(max = 255)
     @Column(name = "ObsOp")
     private String obsOp;
+
+    @ManyToOne
+    private Usuario usuario;
+
+    @ManyToOne
+    private Terceiro terceiro;
 }
--- Textil/src/main/java/br/api/Textil/Usuario/Usuario.java
+++ Textil/src/main/java/br/api/Textil/Usuario/Usuario.java
@@ -1,5 +1,7 @@
 package br.api.Textil.Usuario;
 
+import br.api.Textil.OrdemProducao.OrdemProducao;
+import br.api.Textil.Terceiro.Terceiro;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +9,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -32,4 +36,10 @@ public class Usuario {
     @Size(max = 100)
     @Column(name = "EmailUsuario")
     private String emailUsuario;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", orphanRemoval = true)
+    List<OrdemProducao> OrdemProducao = new ArrayList<>();
+
+    @OneToOne(mappedBy = "usuario", fetch = FetchType.LAZY, orphanRemoval = true)
+    private Terceiro terceiro;
 }
